b05a95a9e7bf92a7ab13fe0808cfaa7c390d9d63
langshiquan <langshiquan@baidu.com>
2017-11-08 18:23:48 +0800

    ci

---
+++ src/main/java/com/baidu/PostBean.java
@@ -0,0 +1,34 @@
+package com.baidu;
+
+/**
+ * Created by langshiquan on 17/11/8.
+ */
+public class PostBean {
+    private String name;
+    private String one;
+    private String two;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOne() {
+        return one;
+    }
+
+    public void setOne(String one) {
+        this.one = one;
+    }
+
+    public String getTwo() {
+        return two;
+    }
+
+    public void setTwo(String two) {
+        this.two = two;
+    }
+}
--- src/main/java/com/baidu/web/HomeController.java
+++ src/main/java/com/baidu/web/HomeController.java
@@ -1,7 +1,11 @@
 package com.baidu.web;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.baidu.PostBean;
 
 /**
  * Created by langshiquan on 17/7/14.
@@ -14,5 +18,11 @@ public class HomeController {
     public String home() {
 
         return "yezhaofeng";
+    }
+
+    @RequestMapping(value = "/testPost", method = RequestMethod.POST)
+    public PostBean testPost(@RequestBody PostBean postBean) {
+
+        return postBean;
     }
 }
