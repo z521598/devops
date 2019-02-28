745725c6d525e9fc503ffd1afc9841558988817e
576506402@qq.com <576506402@qq.com>
2019-02-28 23:34:45 +0800

    aop cglib

---
+++ src/main/java/com/baidu/service/IDemoService.java
@@ -0,0 +1,8 @@
+package com.baidu.service;
+
+/**
+ * Created by Administrator on 2019/2/28.
+ */
+public interface IDemoService {
+    int add(int i,int j);
+}
--- src/main/java/com/baidu/web/HomeController.java
+++ src/main/java/com/baidu/web/HomeController.java
@@ -1,5 +1,12 @@
 package com.baidu.web;
 
+import com.baidu.AopTargetUtils;
+import com.baidu.service.IDemoService;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +19,12 @@ import com.baidu.bean.PostBean;
  */
 
 @RestController
-public class HomeController {
+public class HomeController implements ApplicationContextAware{
 
+    @Autowired
+    private IDemoService demo1;
+
+    private IDemoService demo2;
     @RequestMapping({"", "/"})
     public String home() {
 
@@ -25,4 +36,29 @@ public class HomeController {
 
         return postBean;
     }
+    @RequestMapping("/demo1")
+    public String demo1(){
+        demo1.add(1,2);
+        System.out.println(AopUtils.isAopProxy(demo1));
+        System.out.println(demo1.getClass());
+        return "";
+    }
+
+    @RequestMapping("/demo2")
+    public String demo2(){
+        demo2.add(1,2);
+        System.out.println(AopUtils.isAopProxy(demo2));
+        System.out.println(demo2.getClass());
+        return "";
+    }
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        try {
+            demo2 = (IDemoService) AopTargetUtils.getTarget(applicationContext.getBean("demo"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
